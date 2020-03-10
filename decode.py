ef1c67b3f3f4f27640eb82a8395904034da13d42
gauti1311 <gautam1311@yahoo.com>
2020-03-10 22:26:20 +0100

    camera_matrix added

--- decode.py
+++ decode.py
@@ -7,12 +7,10 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from pylibdmtx.pylibdmtx import decode  
 
-camera_mat2 = np.array([[ 1290.00710, 0.0000, 826.22408], [0.0000, 1289.94789, 613.87523],[0.0000, 0.0000, 1.0000 ]], dtype=float)    ##12*12
-dist_coeff2 = (0.18127, -0.430842, 0.00018424, -0.000338, 0.15621)
-camera_mat1 = np.array([[ 1310.2654, 0.0000, 829.9009], [0.0000, 1309.22745, 617.9510],[0.0000, 0.0000, 1.0000 ]], dtype=float)   ## 10*10  
-dist_coeff1 = (0.2108223, -0.522548, 0.0029681, -0.0011402, 0.32913)
+camera_mat = np.array([[ [999.461170663331, 0, 642.2582577578172,],[ 0, 996.9611451866272, 474.1471906434548],[0.0000, 0.0000, 1.0000 ]], dtype=float)    ##taken from raspicam_package
+dist_coeff = [0.1644274736100891, -0.2717244716038656, -0.002867946281892625, 0, 0]
 axis = np.float32([[20,0,0], [0,20,0], [0,0,10]])     ## 3D_co-ordinates to draw xyz axis
-
+l = 20
 def callback(data):
     global msg
     global new_img 
@@ -38,7 +36,7 @@ def main():
             ## decoding DM with decode func of libdmtx with args no of dmtx, shape and timeout in ms                   
             my_data = decode((img.tobytes(), width, height),shape=0, max_count= 1)  
             ## calculate corners of all dmtx 
-            info, position = my_data[0], my_data[1]   #because origin pixel (0,0) so last pixel will be (1647,1231)
+            info, position = my_data[0], my_data[1]   #because origin pixel (0,0)
             corners = [(position[2],height1-position[3]),(position[0],height1-position[1]),(position[4],height1-position[5]),(position[6],height1-position[7])]  # location of four corners    
             for i in xrange(len(corners)):
                 cv2.circle(img,(int(corners[i][0]),int(all_corners[i][1])),2,(250,0,250),2)  
@@ -51,11 +49,12 @@ def main():
             _, rvec, tvec = cv2.solvePnP(obj_points, img_points, camera_mat, dist_coeff)     
             ## project axis_coord into 2D  
             imgpts, _ = cv2.projectPoints(axis, rvec, tvec, camera_mat, dist_coeff) 
-            ## draw 3D axis o nimage          
+            ## draw 3D axis on image          
             _draw_axis(img, tuple(corners[0]), imgpts) 
             ## rotation matrix using rodrigues   
             rmtx , _ = cv2.Rodrigues(rvec)
             ## normal vector of plane 
+	    print rmtx,tvec
             Nvec= [rmtx[0][2], rmtx[1][2],rmtx[2][2]] 
             showImage(img) 
         else :
